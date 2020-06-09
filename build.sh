85dff9b3e9f1d46b77f6a5e1eee9ea0b7162bc2e
Mayank <mayankchhabra9@gmail.com>
2020-06-09 23:19:41 +0530

    Test 3

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo "Cloning mender-convert"
 git clone https://github.com/mendersoftware/mender-convert.git mender-convert
 
 echo "Getting latest Umbrel OS Image"
-wget https://github.com/getumbrel/umbrel-os/releases/download/v0.0.2-build.10/image_2020-06-08-umbr3l-lite.zip
+wget -q https://github.com/getumbrel/umbrel-os/releases/download/v0.0.2-build.10/image_2020-06-08-umbr3l-lite.zip
 
 echo "Unzipping Umbrel OS Image"
 unzip image_2020-06-08-umbr3l-lite.zip
@@ -18,6 +18,6 @@ sudo apt install $(cat requirements-deb.txt)
 
 echo "Running mender-convert"
 MENDER_ARTIFACT_NAME=release-1 ./mender-convert \
-   --disk-image input/$INPUT_DISK_IMAGE \
+   --disk-image ../$INPUT_DISK_IMAGE \
    --config configs/raspberrypi4_config \
    --overlay rootfs_overlay_demo/
